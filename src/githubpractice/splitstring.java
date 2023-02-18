dd7e6fd720642fe03344e46850eb4123812b71c6
Shashank Shende <shashankshende999>
2023-02-18 20:02:48 +0530

    Frist commit

---
+++ src/githubpractice/splitstring.java
@@ -0,0 +1,17 @@
+package githubpractice;
+
+public class splitstring {
+
+	public static void main(String[] args) {
+		
+		String S="Shashank,Sulbha,Swapnil";
+	String[] sp=S.split(",");
+	for(int i=0;i<sp.length;i++)
+	{
+		System.out.println(sp[i]);
+	}
+	}
+	
+	
+
+}
